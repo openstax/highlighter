4d7b5c49f72429a1d8c20cb26718813c763252b1
tom <tom.g.woodward@gmail.com>
2019-12-03 16:51:39 -0500

    abandon all hope

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
-import { NewHighlightColorEnum } from '@openstax/highlights-client';
+import { NewHighlightColorEnum } from '../highlights-client';
 
-export * from '@openstax/highlights-client';
+export * from '../highlights-client';
 
 export const styleIsColor = (style: string): style is NewHighlightColorEnum =>
   Object.values(NewHighlightColorEnum).includes(style);
