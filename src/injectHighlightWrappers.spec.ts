09f9697f819e9c8375d8a80129898e8c1e3beadb
Beth Shook <es66@rice.edu>
2021-08-20 18:58:14 -0500

    refactor tests

--- src/injectHighlightWrappers.spec.ts
+++ src/injectHighlightWrappers.spec.ts
@@ -2,36 +2,14 @@ import Highlight from './Highlight';
 import injectHighlightWrappers, { DATA_ATTR } from './injectHighlightWrappers';
 
 // tslint:disable: max-line-length
-const firefoxHTML = `
+const testHTML = `
   <div id="reference" class="page-content">
     <div id="main-content">
       <div id="container" data-type="page">
         <p id="test-p">A hardware store sells 16-ft ladders and 24-ft ladders. A window is located 12 feet above the ground. A ladder needs to be purchased that will reach the window from a point on the ground 5 feet from the building.</p>
         <div class="os-figure" id="Figure_01_03_001">
           <figure data-id="Figure_01_03_001" class="small">
-            <span data-type="media" id="fs-id1167339431507" data-alt="A right triangle with a base of 5 feet, a height of 12 feet, and a hypotenuse labeled c">\n
-              <img id="test-img" src="/apps/archive/20210713.205645/resources/b1b5ba97150addc831c534413ca324a72c4e374b" data-media-type="image/jpg" alt="A right triangle with a base of 5 feet, a height of 12 feet, and a hypotenuse labeled c" id="3">\n
-            </span>
-          </figure>
-          <div class="os-caption-container">
-            <span class="os-title-label">Figure </span>
-            <span class="os-number">1</span>
-            <span class="os-divider"> </span>
-            <span class="os-divider"> </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>`;
-
-const chromeHTML = `
-  <div id="reference" class="page-content">
-    <div id="main-content">
-      <div id="container" data-type="page">
-        <p id="test-p">A hardware store sells 16-ft ladders and 24-ft ladders. A window is located 12 feet above the ground. A ladder needs to be purchased that will reach the window from a point on the ground 5 feet from the building.</p>
-        <div class="os-figure" id="Figure_01_03_001">
-          <figure data-id="Figure_01_03_001" class="small">
-            <span data-type="media" id="fs-id1167339431507" data-alt="A right triangle with a base of 5 feet, a height of 12 feet, and a hypotenuse labeled c">
+            <span id="test-span" data-type="media" id="fs-id1167339431507" data-alt="A right triangle with a base of 5 feet, a height of 12 feet, and a hypotenuse labeled c">
               <img id="test-img" src="/apps/archive/20210713.205645/resources/b1b5ba97150addc831c534413ca324a72c4e374b" data-media-type="image/jpg" alt="A right triangle with a base of 5 feet, a height of 12 feet, and a hypotenuse labeled c" id="3">
             </span>
           </figure>
@@ -47,64 +25,68 @@ const chromeHTML = `
   </div>
 `;
 
-// const safariHTML = '';
+describe('inject highlight wrappers', () => {
+  let page: HTMLElement;
+  let img: HTMLElement;
+  let p: HTMLElement;
+  let textNode: Node;
+  let span: HTMLElement;
 
-describe('inject highlight wrappers in firefox', () => {
+  const highlightData = { id: 'some-highlight', content: 'asd', style: 'yellow' };
 
-  it('for highlight ending on an <img>', () => {
-    document.body.innerHTML = firefoxHTML;
-
-    const page = document.getElementById('container')!;
-    const img = document.getElementById('test-img')!;
-    const p = document.getElementById('test-p')!;
-    const textNode = p.childNodes[0];
-    const range: any = {
-      collapse: false,
-      commonAncestorContainer: page,
-      endContainer: img,
-      endOffset: 0,
-      setEndAfter: jest.fn(),
-      setStartBefore: jest.fn(),
-      startContainer: textNode,
-      startOffset: 17,
-    };
-
-    const highlight = new Highlight(range, { id: 'some-highlight', content: 'asd', style: 'yellow' }, { formatMessage: jest.fn() });
-
-    injectHighlightWrappers(highlight);
-    const highlightSpans = document.querySelectorAll(`[${DATA_ATTR}='true']`);
-
-    expect(highlightSpans[0].innerHTML).toMatchSnapshot();
-    expect(highlightSpans[1].innerHTML).toMatchSnapshot();
+  beforeEach(() => {
+    document.body.innerHTML = testHTML;
+    page = document.getElementById('container')!;
+    img = document.getElementById('test-img')!;
+    p = document.getElementById('test-p')!;
+    textNode = p.childNodes[0];
+    span = document.getElementById('test-span')!;
   });
-});
 
-describe('inject highlight wrappers in chrome', () => {
+  describe('for highlight ending on an <img>', () => {
+    it('in chrome', () => {
+      const range: any = {
+        collapse: false,
+        commonAncestorContainer: page,
+        endContainer: img,
+        endOffset: 0,
+        setEndAfter: jest.fn(),
+        setStartBefore: jest.fn(),
+        startContainer: textNode,
+        startOffset: 17,
+      };
+      const highlight = new Highlight(range, highlightData, { formatMessage: jest.fn() });
 
-  it('for highlight ending on an <img>', () => {
-    document.body.innerHTML = chromeHTML;
+      injectHighlightWrappers(highlight);
+      const highlightSpans = document.querySelectorAll(`[${DATA_ATTR}='true']`);
 
-    const page = document.getElementById('container')!;
-    const img = document.getElementById('test-img')!;
-    const p = document.getElementById('test-p')!;
-    const textNode = p.childNodes[0];
-    const range: any = {
-      collapse: false,
-      commonAncestorContainer: page,
-      endContainer: img,
-      endOffset: 0,
-      setEndAfter: jest.fn(),
-      setStartBefore: jest.fn(),
-      startContainer: textNode,
-      startOffset: 17,
-    };
+      expect(highlightSpans[0].innerHTML).toMatchSnapshot();
+      expect(highlightSpans[1].innerHTML).toMatchSnapshot();
+    });
 
-    const highlight = new Highlight(range, { id: 'some-highlight', content: 'asd', style: 'yellow' }, { formatMessage: jest.fn() });
+    it('in firefox', () => {
+      // extra whitespace in firefox
+      span.prepend('\n');
+      span.append('\n');
 
-    injectHighlightWrappers(highlight);
-    const highlightSpans = document.querySelectorAll(`[${DATA_ATTR}='true']`);
+      const range: any = {
+        collapse: false,
+        commonAncestorContainer: page,
+        endContainer: img,
+        endOffset: 0,
+        setEndAfter: jest.fn(),
+        setStartBefore: jest.fn(),
+        startContainer: textNode,
+        startOffset: 17,
+      };
 
-    expect(highlightSpans[0].innerHTML).toMatchSnapshot();
-    expect(highlightSpans[1].innerHTML).toMatchSnapshot();
+      const highlight = new Highlight(range, highlightData, { formatMessage: jest.fn() });
+
+      injectHighlightWrappers(highlight);
+      const highlightSpans = document.querySelectorAll(`[${DATA_ATTR}='true']`);
+
+      expect(highlightSpans[0].innerHTML).toMatchSnapshot();
+      expect(highlightSpans[1].innerHTML).toMatchSnapshot();
+    });
   });
 });
