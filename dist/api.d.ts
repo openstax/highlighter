0f9361b99967831ca17017bcc168d7bf3de03862
Josiah Ivey <josiah.ivey@gmail.com>
2025-11-25 05:53:11 -0800

    dist

---
+++ dist/api.d.ts
@@ -0,0 +1,3 @@
+import { NewHighlightColorEnum } from '@openstax/highlights-client';
+export * from '@openstax/highlights-client';
+export declare const styleIsColor: (style: string) => style is NewHighlightColorEnum;
---
+++ dist/rangeContents.d.ts
@@ -0,0 +1,2 @@
+export declare const rangeContentsString: (range: Range) => string;
+export declare const cloneRangeContents: (range: Range) => DocumentFragment;
