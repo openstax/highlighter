4353f42b8c18818631930685db82e1bdcf820b6d
jomcarvajal <jose.carvajal@edify.cr>
2025-12-29 14:24:04 -0600

    dist

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Highlight_1 = require("./Highlight");
+exports.Highlight = Highlight_1.default;
+const Highlighter_1 = require("./Highlighter");
+const SerializedHighlight_1 = require("./SerializedHighlight");
+exports.SerializedHighlight = SerializedHighlight_1.default;
+exports.default = Highlighter_1.default;
+//# sourceMappingURL=index.js.map
